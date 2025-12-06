b8c40490f86681143b7844c9f21e0f885741cf39
mariaordazr <mariaordazr15@gmail.com>
2025-12-05 22:28:27 -0400

    agregando ig p2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import './Footer.css';
 
 const Footer = () => {
